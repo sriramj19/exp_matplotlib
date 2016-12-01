94ee5ee080067556ade7ab05946ae2339a2deb9e
Orpheus <sriramemailsyou@gmail.com>
2016-12-01 20:44:14 +0530

    Update the source code for better user guidance
    The output procedure is clarified

--- plotGraph.py
+++ plotGraph.py
@@ -1,5 +1,9 @@
+__author__ = "Sriram Jayaraman"
+
 import matplotlib.pyplot as plt
+print ("Enter the values of X-axis: ")
 year=[int(x) for x in (input()).split()]
+print ("Enter the values of Y-axis: ")
 salary=[int(x) for x in input().split()]
 if len(year) == len(salary):
  plt.plot(year,salary)
